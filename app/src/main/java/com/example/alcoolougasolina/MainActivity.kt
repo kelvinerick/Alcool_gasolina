17953574d087084863d7655bcdf63837db2f1848
Luana <luhsousa00@gmail.com>
2024-04-01 19:10:32 -0300

    primeiro commit

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -1,49 +1,86 @@
 package com.example.alcoolougasolina
 
-import android.graphics.Color
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var percentual:Double = 0.7
+    lateinit var etPrecoAlcool: EditText
+    lateinit var etPrecoGasolina: EditText
+    lateinit var btCalc: Button
+    lateinit var textMsg: TextView
+    var swPercentual: Int = 70
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("PDM24.1","No onCreate, $percentual")
 
-        percentual=0.7
-        val btCalc: Button = findViewById(R.id.btCalcular)
-        val textMsg:TextView= findViewById(R.id.textMsg)
-        //btCalc.setBackgroundColor(Color.CYAN)
-        btCalc.setOnClickListener(View.OnClickListener {
-            //código do evento
-            percentual=0.75
-            textMsg.text="Já tenho o percentual"
-            Log.d("PDM24","No btCalcular, $percentual")
-        })
+        etPrecoAlcool = findViewById(R.id.edAlcool)
+        etPrecoGasolina = findViewById(R.id.edGasolina)
+        btCalc = findViewById(R.id.btCalcular)
+        textMsg = findViewById(R.id.textMsg)
+
+        val switch = findViewById<Switch>(R.id.swPercentual)
+
+        switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            swPercentual = if (isChecked) {
+                75
+            } else {
+                70
+            }
+        }
+
+        btCalc.setOnClickListener {
+            val precoAlcoolText = etPrecoAlcool.text.toString()
+            val precoGasolinaText = etPrecoGasolina.text.toString()
+
+            if (precoAlcoolText.isNotEmpty() && precoGasolinaText.isNotEmpty()) {
+                val precoAlcool = precoAlcoolText.toDouble()
+                val precoGasolina = precoGasolinaText.toDouble()
+
+                val percentual = precoAlcool / precoGasolina * 100
+
+                if (percentual <= swPercentual) {
+                    textMsg.text = "Abasteça com alcool"
+                } else {
+                    textMsg.text = "Abasteça com gasolina"
+                }
+
+                Log.d("PDM24", "No btCalcular, $percentual")
+            } else {
+                textMsg.text = "Digite o preço da gasolina e do álcool"
+            }
+        }
     }
-override fun onResume(){
-    super.onResume()
-    Log.d("PDM24","No onResume, $percentual")
-}
-override fun onStart(){
-    super.onStart()
-    Log.v("PDM24","No onStart")
-}
-override fun onPause(){
-    super.onPause()
-    Log.e("PDM24","No onPause")
-}
-override fun onStop(){
-    super.onStop()
-    Log.w("PDM24","No onStop")
-}
-override fun onDestroy(){
-    super.onDestroy()
-    Log.wtf("PDM24","No Destroy")
-}
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("PDM24", "No onResume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v("PDM24", "No onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("PDM24", "No onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("PDM24", "No onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.wtf("PDM24", "No Destroy")
+    }
 }
